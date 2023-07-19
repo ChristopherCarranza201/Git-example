a489b8dce82f5237a2eb9dceda01ebc8496874f7
Christopher Carranza <chriscarranzas71@gmail.com>
2023-07-18 22:21:26 -0700

    Este es el segundo commit de ejemplo

--- ejemplo_fichero.py
+++ ejemplo_fichero.py
@@ -1,8 +1,15 @@
-import random
-list = ["apple" , "pear" , "strawberry"]
-num_fruit = len(list)
-random_choice = random.randint(0, num_fruit -1)
-chosen_fruit = list[random_choice]
-print(chosen_fruit)
 
+# code <nombre_fichero.py> (crear fichero)
+# code . (abrir fichero existente a visual studio)
 
+# (Identificar a un usuario para reflejarlo como archivo ".git" oculto)
+# git config --global user.email "you@example.com"
+# git config --global user.name "Your Name"
+
+# cd <tab_para_seleccionar> (localizar carpeta en disco C:)
+# ls (invocar la librería de archivos de la carpeta consultada)
+
+# git status (checar de la rama "main/master" si se ha realizado algún commit de los ficheros)
+# git add <nombre_fichero.py> (salvar el fichero y poniendo en status "stage")
+# git commit -m "comentario del fichero" (realizar un commit del fichero existente)
+# git branch <nombre_rama_principal> (muestra el nombre de la ramificación del fichero "main")
---
+++ fichero_2.py
@@ -0,0 +1,6 @@
+import random
+list = ["apple" , "pear" , "strawberry"]
+num_fruit = len(list)
+random_choice = random.randint(0, num_fruit -1)
+chosen_fruit = list[random_choice]
+print(chosen_fruit)
