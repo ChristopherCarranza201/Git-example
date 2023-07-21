c29d0806db7b2b7fea540da3e3b4ade518b0bc11
Christopher Carranza <chriscarranzas71@gmail.com>
2023-07-20 17:13:10 -0700

    copia de fichero 1 como respaldo de version 1.0

---
+++ fichero_3.py
@@ -0,0 +1,37 @@
+
+
+# code <nombre_fichero.py> -----------------------------------------------------------------------(crear fichero)
+# code . -----------------------------------------------------------------------------------------(abrir fichero existente a visual studio)
+
+# git config --global user.email "you@example.com"----------------------------------------------- (Identificar a un usuario para reflejarlo como archivo ".git" oculto)
+# git config --global user.name "Your Name"
+
+# git status -------------------------------------------------------------------------------------(checar de la rama "main/master" si se ha realizado algún commit de los ficheros)
+# git add <nombre_fichero.py> --------------------------------------------------------------------(salvar el fichero y poniendo en status "stage")
+# git commit -m "comentario del fichero" ---------------------------------------------------------(realizar un commit del fichero existente)
+# git branch <nombre_rama_principal> -------------------------------------------------------------(muestra el nombre de la ramificación del fichero "main")
+
+# git log ----------------------------------------------------------------------------------------(abrir el commit realizado del fichero)
+# git checkout <tab_para_seleccionar_fichero> ----------------------------------------------------(vuelve a la última versión guardada)
+# git reset <tab_para_seleccionar_fichero> -------------------------------------------------------(indicar modo "unstaged" que el fichero no guardara los cambios que se harán)
+
+# git log --graph --pretty -oneline --------------------------------------------------------------(simplificar historial de commits) 
+# git config --global alias.<name_of_function> ---------------------------------------------------(create a simple command to summon the git log graph...)
+
+# code .gitignore --------------------------------------------------------------------------------(crear un fichero para poder mencionar cuales se ignoran)
+# **/<nombre_fichero_a_ignorar.py> ---------------------------------------------------------------(indicar a .gitignore qué ficheros ignorar del stage)
+
+# git checkout <referencia_log_tree> -------------------------------------------------------------(desplazarse en la versión a través de HEAD)
+# git reset --hard <referencia_log_tree> ---------------------------------------------------------(borra inmediatamente todo avance en la rama
+#-------------------------------------------------------------------------------------------------# a excepción de que es reversible)
+
+# NOTA: IMPORTANTE POR CADA CAMBIO REALIZAR UN COMMIT PARA ASEGURAR EL CAMBIO EN EL LOG
+
+# git reflog --------------------------------------(en caso de no aparecer todos los log de commit borrados, se expondrán utilizandola)
+# git tag <nombre_del_tag> ------------------------(etiquetar nombres al avance actual donde esté el HEAD, main)
+#------------------------------------Otros Comandos Terminal------------------------------------#
+
+# cd <tab_para_seleccionar> ------------------(localizar carpeta en disco C:)
+# ls -----------------------------------------(invocar la librería de archivos de la carpeta consultada)
+# ipconfig /all ------------------------------(mostrar información imprescindible del dispositivo)
+
